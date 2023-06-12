bc0ad03a88b3366f02ebddb4cb81eb89ff9feef3
Dejan1990 <dejan.simo@hotmail.com>
2023-06-12 17:21:01 +0200

    Create routes file

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -12,7 +12,16 @@ class Kernel
     {
         // Create a dispatcher
         $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
-            $routeCollector->addRoute('GET', '/', function() {
+
+            $routes = include BASE_PATH . '/routes/web.php';
+
+            foreach ($routes as $route) {
+                $routeCollector->addRoute(...$route);
+            }
+
+
+
+           /* $routeCollector->addRoute('GET', '/', function() {
                 $content = '<h1>Hello World</h1>';
 
                 return new Response($content);
@@ -22,7 +31,7 @@ class Kernel
                 $content = "<h1>This is Post {$routeParams['id']}</h1>";
 
                 return new Response($content);
-            });
+            }); */
         });
 
         // Dispatch a URI, to obtain the route info
