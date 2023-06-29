90075edc5f840adf8408f8c79dda50518d7c4c33
Dejan1990 <dejan.simo@hotmail.com>
2023-06-29 15:54:03 +0200

    Add flexible handler functionality

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use App\Controller\HomeController;
 use App\Controller\PostsController;
+use GaryClarke\Framework\Http\Response;
 
 return [
     ['GET', '/', [HomeController::class, 'index']],
     ['GET', '/posts/{id:\d+}', [PostsController::class, 'show']],
+    ['GET', '/hello/{name:.+}', function(string $name) {
+        return new Response("Hello $name");
+    }]
 ];
